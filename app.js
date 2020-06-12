f761d59e9eb57d7409bf052fe2f50182207fd0cb
b1mind <brent@1mindsolutions.com>
2020-06-12 05:21:24 -0600

    🐢Console player pieces

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ console.log('hi there')
 
 const gameBoard = document.getElementById('gameBoard')
 const dots = document.querySelectorAll('.dots')
+const playerOneConsole = document.querySelector('.playerOnePieces')
+const playerTwoConsole = document.querySelector('.playerTwoPieces')
+let playerOnePieces = playerOneConsole.children[1].innerHTML
+let playerTwoPieces = playerTwoConsole.children[1].innerHTML
 let playerOne = 'playerOne'
 let playerTwo = 'playerTwo'
 let turn = 0
-let playerOnePieces = 9
-let playerTwoPieces = 9
+let lastX
+let lastY
 let threeMan = null
+let allOnBoard = false
+let error = false
 
 const checkThree = (e, btn, player) => {
 	let x = btn.attributes.cx.value
 	let y = btn.attributes.cy.value
+
 	let playerDots = document.querySelectorAll(`.${player}`)
 	let xMatch = 0
 	let yMatch = 0
+
 	playerDots.forEach(dot => {
 		if (dot.cx.baseVal.value == x) {
 			xMatch++
@@ -23,46 +31,54 @@ const checkThree = (e, btn, player) => {
 			yMatch++
 		}
 	})
+
 	if (xMatch >= 3 || yMatch >= 3) {
 		threeMan = player
 		console.log(`${player} has 3'pimps `)
 	}
+
 	console.log(xMatch, yMatch)
 	console.log(playerDots)
 }
 
 const playerOneAdd = (e, btn) => {
-	btn.classList.replace('empty', 'playerOne')
-	checkThree(e, btn, 'playerOne')
+	btn.classList.replace('empty', playerOne)
+	checkThree(e, btn, playerOne)
 	playerOnePieces--
+	playerOneConsole.children[1].innerHTML = playerOnePieces
 	turn = 1
 	console.log(`Its Turn:${turn}, playerOne: ${playerOnePieces}, playerTwo: ${playerTwoPieces} Del ${threeMan}`)
 }
 
 const playerOneMove = (e, btn, ...args) => {
-	btn.classList.replace('playerOne', 'empty')
+	btn.classList.replace(playerOne, 'empty')
+	lastX = btn.attributes.cx.value
+	lastY = btn.attributes.cy.value
 	playerOnePieces++
 }
 
 const playerOneDel = (e, btn, ...args) => {
-	btn.classList.replace('playerOne', 'empty')
+	btn.classList.replace(playerOne, 'empty')
 	threeMan = false
 }
 
 const playerTwoAdd = (e, btn, ...args) => {
-	btn.classList.replace('empty', 'playerTwo')
-	checkThree(e, btn, 'playerTwo')
+	btn.classList.replace('empty', playerTwo)
+	checkThree(e, btn, playerTwo)
 	playerTwoPieces--
+	playerTwoConsole.children[1].innerHTML = playerTwoPieces
 	turn = 0
 	console.log(`Its Turn:${turn}, playerOne: ${playerOnePieces}, playerTwo: ${playerTwoPieces} Del ${threeMan}`)
 }
 
 const playerTwoMove = (e, btn, ...args) => {
-	btn.classList.replace('playerTwo', 'empty')
+	btn.classList.replace(playerTwo, 'empty')
+	lastX = btn.attributes.cx.value
+	lastY = btn.attributes.cy.value
 	playerTwoPieces++
 }
 const playerTwoDel = (e, btn, ...args) => {
-	btn.classList.replace('playerTwo', 'empty')
+	btn.classList.replace(playerTwo, 'empty')
 	threeMan = false
 }
 
@@ -76,8 +92,8 @@ gameBoard.addEventListener('click', e => {
 			return
 		}
 		//Player One
-	} else if (btn.classList.contains('playerOne')) {
-		if (!threeMan && playerOnePieces <= 0 && turn === 0) {
+	} else if (btn.classList.contains(playerOne)) {
+		if (turn === 0 && playerOnePieces <= 0 && !threeMan) {
 			playerOneMove(e, btn)
 		} else if (threeMan === playerTwo) {
 			playerOneDel(e, btn, playerOne)
@@ -92,6 +108,4 @@ gameBoard.addEventListener('click', e => {
 	}
 })
 
-// check3s(e, btn, player) {e get all player dots, match x or y coords >= 3 } if 3's changeGameState(replace player, empty)
-
-// get all $player $dots that match $btn x or y cords
+//todo in check three put logic for if x or y < 40 points can move if no false move... unless only 3 left on board
